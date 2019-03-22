c0736f3b2baa03adc4fa5809a802e90042f2d1bf
le tong <le.tong@ly.com>
2019-03-22 18:34:00 +0800

    update gitignore

--- DerivedData/Objective-c-Go/Build/Intermediates.noindex/Objective-c-Go.build/Debug-iphonesimulator/Objective-c-Go.build/Objects-normal/x86_64/AppDelegate.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/letong/Desktop/tongle/Objective-c-Go/Objective-c-Go/AppDelegate.m \
-  /Users/letong/Desktop/tongle/Objective-c-Go/Objective-c-Go/GoPrefixHeader.pch \
-  /Users/letong/Desktop/tongle/Objective-c-Go/Objective-c-Go/AppDelegate.h
--- DerivedData/Objective-c-Go/Build/Intermediates.noindex/Objective-c-Go.build/Debug-iphonesimulator/Objective-c-Go.build/Objects-normal/x86_64/BaseTableViewCell.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/letong/Desktop/tongle/Objective-c-Go/Objective-c-Go/Base/BaseTableViewCell.m \
-  /Users/letong/Desktop/tongle/Objective-c-Go/Objective-c-Go/GoPrefixHeader.pch \
-  /Users/letong/Desktop/tongle/Objective-c-Go/Objective-c-Go/Base/BaseTableViewCell.h
